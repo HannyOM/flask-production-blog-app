4f725c51bdcaa7281fffe973420968c04a154b77
HannyOM <hanny0568@gmail.com>
2026-01-10 00:42:03 +0100

    Setup SQLAlchemy.

--- bloggr/__init__.py
+++ bloggr/__init__.py
@@ -1,12 +1,21 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv 
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
+db = SQLAlchemy()                   # Initializes without the app.
+
 def create_app(test_config=None):
     app = Flask(__name__,                   # Tells the app the name of the current Python module where it is located.
                 instance_relative_config=True )                 # Tells the app that the configuration files are relative to the instance folder.
     
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from .models import User
 
     return app
