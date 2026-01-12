03f15e39fd797255eca43bbdb7054b8ac06a0d32
HannyOM <hanny0568@gmail.com>
2026-01-12 21:39:55 +0100

    Updated with SQLAlchemy 2.0

--- bloggr/models.py
+++ bloggr/models.py
@@ -1,24 +1,28 @@
 from . import db
 from flask_security.core import UserMixin, RoleMixin
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 
 roles_users = db.Table(
     "roles_users", 
-    db.Column("role_id", db.Integer(), db.ForeignKey("role.id")),
-    db.Column("user_id", db.Integer(), db.ForeignKey("user.id"))
+    db.Column("role_id", db.Integer(), db.ForeignKey("role.id"), primary_key=True),
+    db.Column("user_id", db.Integer(), db.ForeignKey("user.id"), primary_key=True)
 )
 
+# Creates a "Role" table with three columns.
 class Role(db.Model, RoleMixin):
     __tablename__ = "role"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    description = db.Column(db.String(255))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(db.String(80), unique=True, nullable=False)
+    description: Mapped[str | None] = mapped_column(db.String(255))
 
-class User(db.Model, UserMixin):            # Initializes a "User" table with three columns.
+# Creates a "User" table with five columns.
+class User(db.Model, UserMixin):            
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True) 
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    active = db.Column(db.Boolean())
-    fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False)
-    roles = db.relationship("Role", secondary=roles_users, backref=db.backref("users", lazy="dynamic")) # type: ignore
+    id: Mapped[int] = mapped_column(primary_key=True) 
+    username: Mapped[str] = mapped_column(db.String(20), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(db.String(100), nullable=False)
+    active: Mapped[bool] = mapped_column(default=True)
+    fs_uniquifier: Mapped[str] = mapped_column(db.String(255), unique=True, nullable=False)
+    roles: Mapped[List[Role]] = relationship(secondary=roles_users, backref="users")
