7f59d771aed030b77a2eb6d8c5a0aa6de653be45
HannyOM <hanny0568@gmail.com>
2026-01-12 15:05:06 +0100

    Setup Flask-Security extension.

--- bloggr/__init__.py
+++ bloggr/__init__.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_security.core import Security
+from flask_security.datastore import SQLAlchemyUserDatastore 
+from flask_security.decorators import auth_required
+from flask_security.utils import hash_password
 
 
 load_dotenv()
@@ -11,19 +15,27 @@ load_dotenv()
 db = SQLAlchemy()                   
 migrate = Migrate()
 
-# Application factory
+# Application factodry
 def create_app(test_config=None):
     app = Flask(__name__)           # Identifies the root path for resources like templates and static files.
     
     # Configure application.
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECURITY_PASSWORD_SALT"] = os.environ.get("SECURITY_PASSWORD_SALT")
+    app.config["REMEMBER_COOKIE_SAMESITE"] = "strict"
+    app.config["SESSION_COOKIE_SAMESITE"] = "strict"
+    
     # Initialize extensions (with app object).
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .models import User
+    # Import User model to create user table in database.
+    from .models import User, Role
+
+    # Setup Flask-Security
+    user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+    security = Security(app, user_datastore)
 
     return app
--- bloggr/models.py
+++ bloggr/models.py
@@ -1,8 +1,24 @@
 from . import db
+from flask_security.core import UserMixin, RoleMixin
 
 
-class User(db.Model):            # Initializes a "User" table with three columns.
+roles_users = db.Table(
+    "roles_users", 
+    db.Column("role_id", db.Integer(), db.ForeignKey("role.id")),
+    db.Column("user_id", db.Integer(), db.ForeignKey("user.id"))
+)
+
+class Role(db.Model, RoleMixin):
+    __tablename__ = "role"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):            # Initializes a "User" table with three columns.
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True) 
-    username = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    active = db.Column(db.Boolean())
+    fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False)
+    roles = db.relationship("Role", secondary=roles_users, backref=db.backref("users", lazy="dynamic")) # type: ignore
