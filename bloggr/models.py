5a1c214da47aad53c11cd516550188537b2c039d
HannyOM <hanny0568@gmail.com>
2026-01-12 21:48:51 +0100

    Updated with SQLAlchemy 2.0

--- bloggr/models.py
+++ bloggr/models.py
@@ -13,16 +13,16 @@ roles_users = db.Table(
 # Creates a "Role" table with three columns.
 class Role(db.Model, RoleMixin):
     __tablename__ = "role"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(db.String(80), unique=True, nullable=False)
-    description: Mapped[str | None] = mapped_column(db.String(255))
+    id: Mapped[int] = mapped_column(primary_key=True)           # type: ignore
+    name: Mapped[str] = mapped_column(db.String(80), unique=True, nullable=False)           # type: ignore
+    description: Mapped[str | None] = mapped_column(db.String(255))           # type: ignore
 
 # Creates a "User" table with five columns.
 class User(db.Model, UserMixin):            
     __tablename__ = "user"
-    id: Mapped[int] = mapped_column(primary_key=True) 
-    username: Mapped[str] = mapped_column(db.String(20), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(db.String(100), nullable=False)
-    active: Mapped[bool] = mapped_column(default=True)
-    fs_uniquifier: Mapped[str] = mapped_column(db.String(255), unique=True, nullable=False)
-    roles: Mapped[List[Role]] = relationship(secondary=roles_users, backref="users")
+    id: Mapped[int] = mapped_column(primary_key=True)           # type: ignore
+    username: Mapped[str] = mapped_column(db.String(20), unique=True, nullable=False)           # type: ignore
+    password: Mapped[str] = mapped_column(db.String(100), nullable=False)           # type: ignore
+    active: Mapped[bool] = mapped_column(default=True)           # type: ignore
+    fs_uniquifier: Mapped[str] = mapped_column(db.String(255), unique=True, nullable=False)           # type: ignore
+    roles: Mapped[List[Role]] = relationship(secondary=roles_users, backref="users")           # type: ignore
