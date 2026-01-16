6502701ad8666415ea4d2d0bf72d855487886699
HannyOM <hanny0568@gmail.com>
2026-01-16 10:48:21 +0100

    Added users relationship to Role model and rolesin roles table.

--- bloggr/models.py
+++ bloggr/models.py
@@ -1,16 +1,19 @@
+from __future__ import annotations
 from . import db
 from flask_security.core import UserMixin, RoleMixin
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
-from datetime import datetime, timezone
-from sqlalchemy import DateTime
+from datetime import datetime
+from sqlalchemy import Integer, ForeignKey, DateTime
 
 
+# Roles-Users Core/Association table (Many To Many)
 roles_users = db.Table(
     "roles_users", 
-    db.Column("role_id", db.Integer(), db.ForeignKey("role.id"), primary_key=True),
-    db.Column("user_id", db.Integer(), db.ForeignKey("user.id"), primary_key=True)
+    db.Column("role_id", Integer, ForeignKey("role.id", ondelete="CASCADE"), primary_key=True),
+    db.Column("user_id", Integer, ForeignKey("user.id", ondelete="CASCADE"), primary_key=True)
 )
+
 
 # Creates a "Role" table with three columns.
 class Role(db.Model, RoleMixin):
@@ -18,6 +21,8 @@ class Role(db.Model, RoleMixin):
     id: Mapped[int] = mapped_column(primary_key=True)           # type: ignore
     name: Mapped[str] = mapped_column(db.String(80), unique=True, nullable=False)           # type: ignore
     description: Mapped[str | None] = mapped_column(db.String(255))           # type: ignore
+    users: Mapped[List[User]] = relationship(secondary=roles_users, back_populates="roles")
+
 
 # Creates a "User" table with five columns.
 class User(db.Model, UserMixin):            
@@ -29,4 +34,4 @@ class User(db.Model, UserMixin):
     confirmed_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)           # type: ignore
     active: Mapped[bool] = mapped_column(default=True)           # type: ignore
     fs_uniquifier: Mapped[str] = mapped_column(db.String(255), unique=True, nullable=False)           # type: ignore
-    roles: Mapped[List[Role]] = relationship(secondary=roles_users, backref="users")           # type: ignore
+    roles: Mapped[List[Role]] = relationship(secondary=roles_users, back_populates="users")           # type: ignore
