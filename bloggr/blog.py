d69a0eece82f03ce81d6789b8b258cbc517cb535
HannyOM <hanny0568@gmail.com>
2026-01-19 22:46:06 +0100

    Added edit post, save post and delete post routes.

--- bloggr/blog.py
+++ bloggr/blog.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, abort
 from .models import Post
 from flask_login import current_user, login_required
 from . import db
@@ -9,17 +9,20 @@ from datetime import date
 bp = Blueprint("blog",          # Names the blueprint. 
                __name__)            # Lets the blueprint know where it is defined.
 
+
 # INDEX ROUTE
 @bp.route("/")
 def index():
     all_posts = Post.query.all()
     return render_template("blog/index.html", all_posts=all_posts, user=current_user)
 
+
 # NEW POST ROUTE
 @bp.route("/new", methods=["GET"])
 @login_required
 def new():
     return render_template("blog/new.html")
+
 
 # ADD POST ROUTE
 @bp.route("/add", methods=["GET", "POST"])
@@ -44,3 +47,51 @@ def add():
             db.session.commit()
             return redirect(url_for("blog.index"))
     return render_template("blog/new.html")
+
+
+# EDIT POST ROUTE
+@bp.route("/edit/<int:post_id>", methods=["GET", "POST"])
+@login_required
+def edit(post_id):
+    editing_post = db.get_or_404(Post, post_id)
+    if editing_post.author_id != current_user.id: # type: ignore
+        abort(403)
+    return render_template("blog/edit.html", editing_post=editing_post)
+
+
+# SAVE POST ROUTE
+@bp.route("/save/<int:post_id>", methods=["POST"])
+@login_required
+def save(post_id):
+    editing_post = Post.query.filter_by(id=post_id).first()
+    if request.method == "POST":
+        new_title = request.form.get("new_post_title")
+        new_content = request.form.get("new_post_content")
+        error = None
+
+        if not new_title:
+            error = "Title is required."
+        elif not new_content:
+            error = "Content is required."
+
+        if error is not None:
+            error_msg = flash(error)
+            return render_template("blog/edit.html", editing_post=editing_post, error_msg=error_msg) # type: ignore
+        else:
+            editing_post.title = new_title # type: ignore
+            editing_post.content = new_content # type: ignore
+            db.session.commit()
+    return redirect(url_for("blog.index"))
+
+
+# DELETE POST ROUTE
+@bp.route("/delete/<int:post_id>", methods=["GET"])
+@login_required
+def delete(post_id):
+    deleting_post = db.get_or_404(Post, post_id)
+    if deleting_post.author_id != current_user.id: # type: ignore
+        abort(403)
+    else:    
+        db.session.delete(deleting_post)
+        db.session.commit()
+    return redirect(url_for("blog.index"))
