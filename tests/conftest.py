10f1b6ada21cd009802f4c803a54ad59835c4ae3
HannyOM <hanny0568@gmail.com>
2026-01-21 20:09:46 +0100

    Initial test commits.

---
+++ tests/conftest.py
@@ -0,0 +1,73 @@
+import pytest
+from testcontainers.postgres import PostgresContainer
+from bloggr import create_app 
+from bloggr import db as _db
+from flask_security.utils import hash_password
+from bloggr.models import User
+
+
+# Start the Postgres Container once for the entire test run
+@pytest.fixture(scope="session")
+def postgres_server():
+    with PostgresContainer("postgres:16-alpine") as postgres:
+        yield postgres
+
+# Set app fixture to use the Docker container's URI.
+@pytest.fixture
+def app(postgres_server):
+    # Get the dynamic connection string from the container.
+    # e.g., postgresql+psycopg2://test:test@localhost:54321/test
+    database_uri = postgres_server.get_connection_url()
+    
+    app = create_app({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": database_uri,
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+        "WTF_CSRF_ENABLED": False
+    })
+
+    with app.app_context():         # Enters the Flask application context.
+        _db.create_all()  # Setup tables for this specific test.
+        yield app           # Yields the app instance to tests (pauses here while tests run).
+        _db.session.remove()            # Cleans up the database session.
+        _db.drop_all()    # Wipe tables so the next test starts clean.
+
+@pytest.fixture
+def db(app):
+    return _db
+
+@pytest.fixture
+def client(app):            # Creates a test client (simulates HTTP requests to the Flask app).
+    return app.test_client()
+
+@pytest.fixture
+def create_user(db):
+    username, password, email, confirmed_at, fs_uniquifier = "test_username", "test_password", "testmail@gmail.com", "2026-01-21 13:00:22.479989+01", "acbc5Feafb4E6bc7b1ae6de5e07b2d89"
+    hashed_password = hash_password(password)
+    user = User(username=username, password=hashed_password, email=email, confirmed_at=confirmed_at, fs_uniquifier=fs_uniquifier)
+    db.session.add(user)
+    db.session.commit()
+    return username, password, user, email, confirmed_at, fs_uniquifier
+
+@pytest.fixture
+def create_user2(db):
+    username2, password2, email2, confirmed_at2, fs_uniquifier2 = "test_username2", "test_password2", "testmail2@gmail.com", "2025-01-21 13:34:22.479989+01", "scbs3Feafb4E6bc7f2ae6de5e07b2d89"
+    hashed_password2 = hash_password(password2)
+    user2 = User(username=username2, password=hashed_password2, email=email2, confirmed_at=confirmed_at2, fs_uniquifier=fs_uniquifier2 )
+    db.session.add(user2)
+    db.session.commit()
+    return username2, password2, user2, email2, confirmed_at2, fs_uniquifier2
+
+class AuthActions:
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, email="testmail@gmail.com", password="test_password"):
+        return self._client.post("/login", data={"email": email, "password": password})
+
+    def logout(self):
+        return self._client.get("/logout")
+    
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
---
+++ tests/test_blog.py
@@ -0,0 +1,126 @@
+import pytest
+from bloggr.models import Post
+from datetime import date
+
+
+# Test to see if the index page works.
+def test_index(client, create_user, auth):
+    response = client.get("/")
+    assert b"New Post" in response.data
+
+    auth.login()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Welcome" in response.data
+
+
+# Test to see if the app sends the unauthenticated user to the login page.
+@pytest.mark.parametrize(("path", "method"), (("/new", "GET"), 
+                                              ("/add", "POST"), 
+                                              ("/edit/2", "GET"), 
+                                              ("/save/2", "POST"), 
+                                              ("/delete/2", "GET")))
+def test_login_required(client, path, method):
+    if method == "GET":
+        response = client.get(path)
+    else:
+        response = client.post(path)
+    assert response.headers["Location"].startswith("/login")
+
+
+# Test to see if only the author of a post can edit it.
+def test_author_required(app, db, create_user, create_user2, auth, client):         # Tests that the author_id is required to determine who has permission to edit or delete a post. 
+    with app.app_context():
+        username, password, user, email, confirmed_at, fs_uniquifier = create_user          # Creates User1.
+        username2, password2, user2, email2, confirmed_at2, fs_uniquifier2 = create_user2          # Creates User2.
+
+        post = Post(            # Creates a post from User1
+                title = "User1 Blog Post", # type:ignore
+                content = "This is actually his first blog post.", # type:ignore
+                author_id = user.id, # type:ignore
+                date = date.today() # type:ignore
+            )
+        db.session.add(post)
+        db.session.commit()
+        retrieved_post = Post.query.filter_by(id=1).first()
+        retrieved_post.author_id = user2.id # type: ignore             # Changes the author_id of User1's post(User1's id) to User2's id.
+        db.session.commit()
+
+    auth.login()            # Logs in User1
+    assert client.get("/edit/1").status_code == 403         # Asserts that if User1 tries to edit the post(whose author_id was reassigned User2's id), User1 will be forbidden.
+    assert client.get("/delete/1").status_code == 403           # Asserts that if User1 tries to delete the post(whose author_id was reassigned User2's id), User1 will be forbidden.
+
+# To see if the path exists.
+@pytest.mark.parametrize(("path"), (("/edit/2"),            # Tests that a path must exist to be accessed.
+                                    ("/delete/2")))
+def test_exists_required(client, create_user, auth, path):          
+    auth.login()
+    response = client.get(path)
+    assert response.status_code == 404          # Asserts that if the path does not exist, a 404 status code is returned.
+
+
+# Test to see if adding post works.
+def test_add_post(client, create_user, auth, app):
+    auth.login()
+    response = client.get("/new")
+    assert response.status_code == 200
+
+    client.post("/add", data={"post_title" : "The Post title",
+                              "post_content" : "The Post content"})
+    with app.app_context():
+        count = Post.query.count()
+        assert count == 1
+
+
+# Test to see if editing post works.
+def test_edit_post(client, create_user, auth, app, db):
+    auth.login()
+    client.post("/add", data={"post_title" : "The post title",
+                              "post_content" : "The post content"})
+    response = client.get("/edit/1")
+    assert response.status_code == 200
+
+    client.post("/save/1", data={"new_post_title" : "The new post title",
+                                 "new_post_content" : "The new post content"})
+    with app.app_context():
+        edited_post = db.get_or_404(Post, 1)
+        assert edited_post.title == "The new post title"
+
+
+# Test to see if a new post is properly validated before it is added.
+@pytest.mark.parametrize(("title", "content", "message"),(("", "", b"Title is required."),
+                                                          ("Test post title", "", b"Content is required.")))
+def test_add_post_validate(client, create_user, auth, title, content, message):
+    auth.login()
+    response = client.post("/add", data={"post_title" : title,
+                                         "post_content" : content})
+    assert message in response.data
+    print(response)
+
+
+# Test to see if a post is properly validated before it is edited.
+@pytest.mark.parametrize(("new_title", "new_content", "message"),(("", "", b"Title is required."),
+                                                          ("New test post title", "", b"Content is required.")))
+def test_edit_post_validate(client, create_user, auth, new_title, new_content, message):
+    auth.login()
+    client.post("/add", data={"post_title" : "The post title",
+                              "post_content" : "The post content"})
+    response = client.post("/save/1", data={"new_post_title" : new_title,
+                                            "new_post_content" : new_content})
+    assert message in response.data
+    print(response)
+
+
+# Test to see if deleting post works.
+def test_delete(client, create_user, auth, app):
+    auth.login()
+    client.post("/add", data={"post_title" : "The post title",
+                              "post_content" : "The post content"})
+    with app.app_context():
+        count = Post.query.count()
+        assert count == 1
+    
+    client.get("/delete/1")
+    with app.app_context():
+        count = Post.query.count()
+        assert count == 0
