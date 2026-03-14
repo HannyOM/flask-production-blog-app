df5fe0994845ee6e86c5ac9259c5e5f5afdcaa5c
Hanny Abubakar <hanny0568@gmail.com>
2026-03-14 23:47:33 +0100

    Switch to port 465 SSL for Gmail SMTP

--- bloggr/__init__.py
+++ bloggr/__init__.py
@@ -35,8 +35,9 @@ class ProductionConfig:
     SECURITY_CONFIRMABLE = True
     SECURITY_POST_CONFIRM_VIEW = "/login"
     MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
