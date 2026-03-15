36cd12f9aa9df493d4966258bc4a5bf2f8819653
Hanny Abubakar <hanny0568@gmail.com>
2026-03-15 15:55:32 +0100

    Fix user registration hanging by removing db.session.refresh

--- bloggr/roles.py
+++ bloggr/roles.py
@@ -11,16 +11,14 @@ def setup_roles_signals(app):
     @user_registered.connect_via(app)
     def assign_default_role(sender, user, **extra):
         try:
-            db.session.refresh(user)
-            
             role = Role.query.filter_by(name="editor").first()
-            
+
             if not role:
                 role = Role(name="editor", description="can create and edit posts.")
                 db.session.add(role)
                 db.session.commit()
                 role = Role.query.filter_by(name="editor").first()
-            
+
             if role not in user.roles:
                 user.roles.append(role)
                 db.session.commit()
