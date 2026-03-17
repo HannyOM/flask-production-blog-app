99c992cc0f9ed4ef5e3f30fcf774bd9291e9dd03
Hanny Abubakar <hanny0568@gmail.com>
2026-03-17 02:40:13 +0100

    Add TipTap WYSIWYG editor for blog posts

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    outDir: "bloggr/static",
+    lib: {
+      entry: resolve(__dirname, "bloggr/static/js/editor.js"),
+      name: "Editor",
+      fileName: "editor",
+      formats: ["iife"],
+    },
+    rollupOptions: {
+      input: {
+        editor: resolve(__dirname, "bloggr/static/js/editor.js"),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "bloggr"),
+    },
+  },
+});
